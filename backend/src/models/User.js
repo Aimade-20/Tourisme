b0fdb43237d593a2e02ed5bcac510fdcff5bfc4a
Aimade-20 <imadesafsafi8@gmail.com>
2026-09-01 16:26:50 +0100

    first commit

---
+++ backend/src/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    unique: true,
+  },
+  password: { 
+    type: String, 
+    required: true,
+},
+  role: {
+    type: String,
+    require: true,
+    enum: ["admin", "user", "guide"],
+    default: "user",
+  },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
